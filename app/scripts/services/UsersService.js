5a444ece2360497df8c6e658effe33119799a64b
Brady Georgen <pbradygeorgen@me.com>
2013-11-12 14:38:39 -0600

    changed urls to omit localhost

--- app/scripts/services/UsersService.js
+++ app/scripts/services/UsersService.js
@@ -4,14 +4,14 @@ angular.module('messageBoothApp')
   .service('UsersService', function Users($http) {
     var usersService = {
 	    get: function() {
-	      var promise = $http.get('http://localhost:3000/users.json').then(function (response) {
+	      var promise = $http.get('/users.json').then(function (response) {
 	        console.log(response);
 	        return response.data;
 	      });
 	      return promise;
 	    },
 		put: function(id, user) {
-				var promise = $http.put('http://localhost:3000/users/'+id+'.json', user).then(function (response) {
+				var promise = $http.put('/users/'+id+'.json', user).then(function (response) {
 	        	console.log("User:");
 				console.log(user);
 	        	return response.data;
